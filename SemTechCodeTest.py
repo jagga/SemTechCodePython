4ad84e343576d452247b935b1a65a1cc1ffca8ef
Jagga Muddasani <saibaba.jagan@gmail.com>
2025-03-31 17:31:27 -0400

    SemTechCodePython unittest scripts changes

--- SemTechCodeTest.py
+++ SemTechCodeTest.py
@@ -3,58 +3,44 @@ import SemTechCodePython
  
 class  PopulationMainTest(unittest.TestCase):
 
-    lineStrmObj =  SemTechCodePython.LineStreamProc()
-    lines = lineStrmObj.fileLines("population_2019.csv")
-    Mgroups, MminPop, MmaxPop, MsumPop = lineStrmObj.getPopulationByDept(lines)
+    def getPopulationDetails(self):
+        self.lineStrmObj =  SemTechCodePython.LineStreamProc()
+        lines = self.lineStrmObj.fileLines("population_2019.csv")
+        mgroups, mminpop, mmaxpop, msumpop = self.lineStrmObj.getPopulationByDept(lines)
+        return mgroups, mminpop, mmaxpop, msumpop
 
     def test_totalPopulationByDepartment(self):
-        lineStrmObj =  SemTechCodePython.LineStreamProc()
-        lines = lineStrmObj.fileLines("population_2019.csv")
-        Mgroups, MminPop, MmaxPop, MsumPop = lineStrmObj.getPopulationByDept(lines)
-
+        mgroups, mminpop, mmaxpop, msumpop = self.getPopulationDetails()
         lines = self.lineStrmObj.fileLines("totpopbydept.csv")
-        Tgroups, TminPop, TmaxPop, TsumPop = lineStrmObj.getPopulationByDept(lines)
-
-        self.assertEqual(MsumPop.get("MANCHE").p, TsumPop.get("MANCHE").p)
-        self.assertEqual(MsumPop.get("GIRONDE").p, TsumPop.get("GIRONDE").p)
-        self.assertEqual(MsumPop.get("COTE-D'OR").p, TsumPop.get("COTE-D'OR").p)
-        self.assertEqual(MsumPop.get("COTES-D'ARMOR").p, TsumPop.get("COTES-D'ARMOR").p)
-        self.assertNotEqual(MsumPop.get("CALVADOS").p, TsumPop.get("CALVADOS").p)
-        self.assertNotEqual(MsumPop.get("CANTAL"), TsumPop.get("CANTAL"))
-        self.assertNotEqual(MsumPop.get("CHARENTE"), TsumPop.get("CHARENTE"))
-        self.assertNotEqual(MsumPop.get("CHARENTE-MARITIME"), TsumPop.get("CHARENTE-MARITIME"))
+        tgroups, tminpop, tmaxpop, tsumpop = self.lineStrmObj.getPopulationByDept(lines)
+        self.assertEqual(msumpop.get("MANCHE").p, tsumpop.get("MANCHE").p)
+        self.assertEqual(msumpop.get("GIRONDE").p, tsumpop.get("GIRONDE").p)
+        self.assertEqual(msumpop.get("COTE-D'OR").p, tsumpop.get("COTE-D'OR").p)
+        self.assertEqual(msumpop.get("COTES-D'ARMOR").p, tsumpop.get("COTES-D'ARMOR").p)
+        self.assertNotEqual(msumpop.get("CALVADOS").p, tsumpop.get("CALVADOS").p)
+        self.assertNotEqual(msumpop.get("CANTAL"), tsumpop.get("CANTAL"))
+        self.assertNotEqual(msumpop.get("CHARENTE"), tsumpop.get("CHARENTE"))
+        self.assertNotEqual(msumpop.get("CHARENTE-MARITIME"), tsumpop.get("CHARENTE-MARITIME"))
 
 
     def test_maxPopulationByDepartment(self):
-        lineStrmObj =  SemTechCodePython.LineStreamProc()
-        lines = lineStrmObj.fileLines("population_2019.csv")
-        Mgroups, MminPop, MmaxPop, MsumPop = lineStrmObj.getPopulationByDept(lines)
-
-        self.lines =  self.lineStrmObj.fileLines("maxpopbydept.csv")
-        Tgroups, TminPop, TmaxPop, TsumPop = lineStrmObj.getPopulationByDept(lines)
-
-        self.lines = self.lineStrmObj.fileLines("maxpopbydept.csv")
-        self.maxPopByDeptT = self.lineStrmObj.getPopulationByDept(lines)
-
-        self.assertEqual(MmaxPop.get("MANCHE").p, TmaxPop.get("MANCHE").p)
-        self.assertEqual(MmaxPop.get("GIRONDE").p, TmaxPop.get("GIRONDE").p)
-        self.assertEqual(MmaxPop.get("COTE-D'OR").p, TmaxPop.get("COTE-D'OR").p)
-        self.assertEqual(MmaxPop.get("COTES-D'ARMOR").p, TmaxPop.get("COTES-D'ARMOR").p)
+        mgroups, mminpop, mmaxpop, msumpop = self.getPopulationDetails()
+        lines =  self.lineStrmObj.fileLines("maxpopbydept.csv")
+        tgroups, tminpop, tmaxpop, tsumpop = self.lineStrmObj.getPopulationByDept(lines)
+        self.assertEqual(mmaxpop.get("MANCHE").p, tmaxpop.get("MANCHE").p)
+        self.assertEqual(mmaxpop.get("GIRONDE").p, tmaxpop.get("GIRONDE").p)
+        self.assertEqual(mmaxpop.get("COTE-D'OR").p, tmaxpop.get("COTE-D'OR").p)
+        self.assertEqual(mmaxpop.get("COTES-D'ARMOR").p, tmaxpop.get("COTES-D'ARMOR").p)
 
 
     def test_minPopulationByAllDepartments(self):
-        lineStrmObj =  SemTechCodePython.LineStreamProc()
-        lines = lineStrmObj.fileLines("population_2019.csv")
-        Mgroups, MminPop, MmaxPop, MsumPop = lineStrmObj.getPopulationByDept(lines)
-
-        self.lines =  self.lineStrmObj.fileLines("minpopbyalldept.csv")
-        Tgroups, TminPop, TmaxPop, TsumPop = lineStrmObj.getPopulationByDept(lines)
-
-
-        minCities = min(MminPop.keys());
-        minCitiesT = min(TminPop.keys());
-        print(minCities, minCitiesT)
-        self.assertEqual(minCities, minCitiesT)
+        mgroups, mminpop, mmaxpop, msumpop = self.getPopulationDetails()
+        lines =  self.lineStrmObj.fileLines("minpopbyalldept.csv")
+        tgroups, tminpop, tmaxpop, tsumpop = self.lineStrmObj.getPopulationByDept(lines)
+        mincities = min(mminpop.keys())
+        mincitiest = min(tminpop.keys())
+        print(mincities, mincitiest)
+        self.assertEqual(mincities, mincitiest)
 
 
 if __name__ == '__main__':
