aac9fd8d1c4b3b3cad0154a600ebb0fcf62aa9d9
Jagga Muddasani <saibaba.jagan@gmail.com>
2025-03-31 18:04:07 -0400

    SemTechCodePython removed comments

--- SemTechCodePython.py
+++ SemTechCodePython.py
@@ -139,38 +139,6 @@ class LineStreamProc:
         print(popMini, " is the Minimum Population for cities " , popmin[popMini], " " )
         return  groups, minPop, maxPop, sumPop
 
-#     # Employubg Stream of lines from input file get Max Population for Each Department
-#     def getMaxPopulationByDept(lines: List[String]): Predef.Map[String, Int] = {
-#       val maxPopByDept = lines
-#         .map(line => new PopObj(line))
-#         .filter(e => e.p > -1)
-#         .groupMapReduce(e => e.c)(e => e.p)((a, b) => math.max(a, b));
-#       return maxPopByDept;
-#     }
-#     # Employubg Stream of lines from input file get Total Population for Each Department
-#     def getTotalPopulationByDept(lines: List[String]): Predef.Map[String, Int] = {
-#       val totalPopByDept = lines
-#         .map(line => new PopObj(line))
-#         .filter(e => e.p > -1)
-#         .groupMapReduce(_.c)(e => e.p)((a, b) => math.addExact(a, b));
-#
-#       return totalPopByDept
-#     }
-#   var minKey:Int = 0;
-#   # Employubg Stream of lines from input file get Minimum Population for All Departments
-#     def getMinPopulationForAllDepts(lines: List[String]) : scala.collection.immutable.Map[Int, List[String]]  = {
-#       var minPopAllDept = lines
-#         .map(line => new PopObj(line))
-#         .filter(e => e.p > 0)
-#         .groupMapReduce(_.c)(e => e.p)((a, b) => math.min(a, b))
-#         .groupMap(e => (e._2))(e => e._1)
-#         .map(e => (e._1, e._2.toList));
-#
-#       # println (minPopAllDept.toString());
-#        return minPopAllDept;
-#     }
-
-
 if __name__ == "__main__":
     lineproc = LineStreamProc();
     lines = lineproc.fileLines(sys.argv[1])
